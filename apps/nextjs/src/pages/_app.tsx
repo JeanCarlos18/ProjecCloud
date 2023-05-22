07b381bccedd7edba881d8d15a72d4f8cd07e497
Miqueas Bernartd <santiagomiqueas.10@gmail.com>
2023-05-22 00:52:09 -0300

    database tweaking, added navigation bar

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -3,10 +3,12 @@ import "../styles/globals.css";
 import type { AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import { trpc } from "../utils/trpc";
+import { NavBar } from "../components/NavBar";
 
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <NavBar />
       <Component {...pageProps} />
     </ClerkProvider>
   );
